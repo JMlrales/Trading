a8516e9521515431995b1d912dd64535113bf62b
Jose Morales <dejesus.morales@iteso.mx>
2025-08-26 18:50:53 -0600

    Calculo de factoriales mayores a 1

--- Calculadora.py
+++ Calculadora.py
@@ -2,4 +2,6 @@ def fact(n):
          if n<0:
                  return('El resultado es indefinido')
          elif n<=1:
-                 return(1)
+                 return 1
+         else:
+                 return n*fact*(n-1)
